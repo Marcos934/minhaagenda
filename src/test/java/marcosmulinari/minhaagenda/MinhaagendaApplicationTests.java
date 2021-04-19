f1532ad5a64582979a7a4c0686f9148db1f56d5c
Marcos Mulinari <marcos.mulinari97@gmail.com>
2021-04-19 15:36:39 +0000

    Iniciando desenvolvimento API minha agenda

---
+++ src/test/java/marcosmulinari/minhaagenda/MinhaagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package marcosmulinari.minhaagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
